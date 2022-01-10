c12ad33888c3563bc41dcdae964e2b6ed0a69a7b
Pedro Lucas <pedrolucaspbi@gmail.com>
2022-01-10 19:42:36 -0300

    Gerador de P.A

---
+++ Projeto/Geradordepa.py
@@ -0,0 +1,16 @@
+print('=============== \033[1;32mGERADOR DE P.A\033[m ===============')
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print('{} '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos voce quer mostrar a mais? '))
+print('PA finalizada com {} termos mostrados'.format(total))
